798671f097db3dfd55b403e7de5cd45d97888a99
Evan Genest <genest.1@osu.edu>
2018-08-05 11:30:14 -0500

    Write my own script to add all of Brandon's file tree's.  Found this neccessary after I goofed pasting them one by one while listening to him present.

---
+++ buildBrandon.sh
@@ -0,0 +1,15 @@
+cal 
+mkdir -p src/api && touch src/api/app.js
+mkdir -p src/api/controllers && touch src/api/controllers/ProductsController.js
+mkdir -p src/api/routes && touch src/api/routes/ProductRoutes.js
+mkdir -p src/api/models && touch src/api/models/Product.js
+mkdir -p src/web/html && touch src/web/html/index.html
+mkdir -p src/web/styles && touch src/web/styles/app.scss
+ mkdir -p src/web/scripts && touch src/web/scripts/app.jsx
+mkdir -p src/web/scripts/components/products && touch src/web/scripts/components/products/index.jsx
+mkdir -p src/web/scripts/containers && touch src/web/scripts/containers/products.js
+ mkdir -p src/web/scripts/actions && touch src/web/scripts/actions/products.js
+mkdir -p src/web/scripts/reducers && touch src/web/scripts/reducers/products.js && touch src/web/scripts/reducers/index.js
+ mkdir -p src/web/scripts/enums && touch src/web/scripts/enums/products.js && touch src/web/scripts/enums/index.js
+ mkdir -p src/web/scripts/routing && touch src/web/scripts/routing/dproducts.js && touch src/web/scripts/routing/index.js
+cal 1960
